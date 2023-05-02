d64e8089ea905dfe52f34e4a95afbaf4dbac8b0f
revyrob <kayle.robson@gmail.com>
2023-05-01 21:46:21 -0700

    adding stying to homepage

--- operation-mother-earth_react/src/components/BigGameBtn/BigGameBtn.js
+++ operation-mother-earth_react/src/components/BigGameBtn/BigGameBtn.js
@@ -3,10 +3,12 @@ import "./BigGameBtn.scss";
 function BigGameBtn({ img, alt, text, imgLrg, altLrg }) {
   return (
     <button className="bigGameBtn">
-      <h2 className="bigGameBtn--text--top">{text}</h2>
       <img className="bigGameBtn--imgLrg" src={imgLrg} alt={altLrg} />
-      <img className="bigGameBtn--img" src={img} alt={alt} />
-      <h2 className="bigGameBtn--text--bottom">{text}</h2>
+      <div className="bigGameBtn__wrapper">
+        <h2 className="bigGameBtn--text--top">{text}</h2>
+        <img className="bigGameBtn--img" src={img} alt={alt} />
+        <h2 className="bigGameBtn--text--bottom">{text}</h2>
+      </div>
     </button>
   );
 }
